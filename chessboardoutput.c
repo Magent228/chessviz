71554fff333a5e43759a94e74113635ddeefb3c7
snoweee322 <diman199909@gmail.com>
2018-02-21 19:41:13 +0700

    Added base constructions

---
+++ chessboardoutput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{		
+	for (i = 0; i <= 9; i++)
+	{
+		printf("\n\n");
+		for (j = 0; j <= 9; j++)
+		printf(" %c ",A[i][j]);
+	}
+	system("PAUSE");
+	return 0;
+}
+
