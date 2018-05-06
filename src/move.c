551c7a9aa5ab08e5d9d26c1864b5ebef6eec3275
Dmitry <diman199909@gmail.com>
2018-05-06 22:52:21 +0700

    Add all move types for chess figures

--- src/move.c
+++ src/move.c
@@ -38,7 +38,7 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && ((y-1 == y1 && x-1 == x1) || (y-1 == y1 && x+1 == x1)))
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 65 && A[y1][x1] <= 90 && ((y-1 == y1 && x-1 == x1) || (y-1 == y1 && x+1 == x1)))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
@@ -56,7 +56,7 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && ((y+1 == y1 && x-1 == x1) || (y+1 == y1 && x+1 == x1)))
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 97 && A[y1][x1] <= 122 && ((y+1 == y1 && x-1 == x1) || (y+1 == y1 && x+1 == x1)))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
@@ -67,14 +67,14 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 	}
 	if (A[y][x] == 'r') //white rooks
 	{
-		if (A[y1][x1] == ' ' && x == x1)
+		if (A[y1][x1] == ' ' && (x == x1 || y == y1))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && (x == x1 || y == y1))
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 65 && A[y1][x1] <= 90 && (x == x1 || y == y1))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
@@ -85,14 +85,14 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 	}
 	if (A[y][x] == 'R') //black rooks
 	{
-		if (A[y1][x1] == ' ' && x == x1)
+		if (A[y1][x1] == ' ' && (x == x1 || y == y1))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && (x == x1 || y == y1))
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 97 && A[y1][x1] <= 122 && (x == x1 || y == y1))
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
@@ -110,7 +110,7 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && (pow(x1-x, 2) + pow(y1-y, 2)) == 5)
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 65 && A[y1][x1] <= 90 && (pow(x1-x, 2) + pow(y1-y, 2)) == 5)
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
@@ -128,7 +128,115 @@ bool move(int y, int x, int y1, int x1, char (*A)[10])
 			board (A);
 			return true;
 		}
-		if (A[y1][x1] != ' ' && (pow(x1-x, 2) + pow(y1-y, 2)) == 5)
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 97 && A[y1][x1] <= 122 && (pow(x1-x, 2) + pow(y1-y, 2)) == 5)
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'b') // bishop white (elephant)
+	{
+		if (A[y1][x1] == ' ' && abs(y1-y) == abs(x1-x))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 65 && A[y1][x1] <= 90 && abs(y1-y) == abs(x1-x))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'B') // bishop black (elephant)
+	{
+		if (A[y1][x1] == ' ' && abs(y1-y) == abs(x1-x))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && A[y1][x1] >= 97 && A[y1][x1] <= 122 && abs(y1-y) == abs(x1-x))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'q') // white queen (ferz')
+	{
+		if (A[y1][x1] == ' ' && (x == x1 || y == y1 || (abs(y1-y) == abs(x1-x))))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && (x == x1 || y == y1 || (abs(y1-y) == abs(x1-x))) && A[y1][x1] >= 65 && A[y1][x1] <= 90)
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'Q') // black queen (ferz')
+	{
+		if (A[y1][x1] == ' ' && (x == x1 || y == y1 || (abs(y1-y) == abs(x1-x))))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && (x == x1 || y == y1 || (abs(y1-y) == abs(x1-x))) && A[y1][x1] >= 97 && A[y1][x1] <= 122)
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'k') // white king
+	{
+		if (A[y1][x1] == ' ' && (abs(y1-y) == 1 || abs(x1-x) == 1))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && (abs(y1-y) == 1 || abs(x1-x) == 1) && A[y1][x1] >= 65 && A[y1][x1] <= 90)
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		return false;
+	}
+	if (A[y][x] == 'K') // black king
+	{
+		if (A[y1][x1] == ' ' && (abs(y1-y) == 1 || abs(x1-x) == 1))
+		{
+			A[y1][x1] = A[y][x];
+			A[y][x] = ' ';
+			board (A);
+			return true;
+		}
+		if (A[y1][x1] != ' ' && (abs(y1-y) == 1 || abs(x1-x) == 1) && A[y1][x1] >= 97 && A[y1][x1] <= 122)
 		{
 			A[y1][x1] = A[y][x];
 			A[y][x] = ' ';
